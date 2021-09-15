61b39395f865260a93778666201e2c5590f3cedf
Ruben Castañeda <ruben@pixeling.co>
2021-09-15 14:20:51 -0500

    FEAT(Games)PaginationAndGamesFilters

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,5 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Request, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
-import { ApiBasicAuth, ApiBearerAuth, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Query,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Request,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+  DefaultValuePipe,
+  ParseIntPipe
+} from '@nestjs/common';
+import {
+  ApiBasicAuth,
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateGameDto } from './dto/createGame.dto';
 import { RolesGuard } from './../auth/guards/role.guard';
 import { JwtAuthGuard } from './../auth/guards/jwtAuth.guard';
@@ -13,136 +35,163 @@ import { GameEntity } from './entities/game.entity';
 import { UpdateGameDto } from './dto/updateGame.dto';
 import { UploadFileNestjs } from '../utils/decorators/UploadFile.decorator';
 import { ValidatedFileConfig } from 'src/utils/config/validatedFile.config';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 
 @ApiTags('Game')
 @Controller('game')
 export class GameController {
+  constructor(private readonly gameService: GameService) {}
 
-    constructor(
-        private readonly gameService: GameService
-    ) { }
-
-    /* STORE  [ ONLY DEV ]*/
-    @Post('')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiConsumes('multipart/form-data')
-    @ApiOperation({ summary: 'Create a game [ONLY DEV]' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Developer)
-    @UseInterceptors(
-        FileFieldsInterceptor(
-            [{ name: 'image', maxCount: 1, }, { name: 'icon', maxCount: 1, }, { name: 'screenshots', maxCount: 5 }],
-            ValidatedFileConfig
-        )
-    )
-    @UploadFileNestjs('image', 'icon', 'screenshots')
-    async store(
-        @Request() req: any,
-        @Body() CreateGameDto: CreateGameDto,
-        @GetUser() user: UserEntity,
-    ): Promise<any>{
-        return await this.gameService.store(CreateGameDto, user, req);
-    }
-
-    /* GET [ ALL ]*/
-    @Get(':id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Get a game [ALL]' })
-    async get(@Param('id') id: number): Promise<GameEntity>{
-        return await this.gameService.get(id);
-    }
-
-    
-    /* GET GAME USERS [ ALL ]*/
-    @Get('user/:id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Get all games from user [ALL]' })
-    async getGamesFromDev(@Param('id') id: UserEntity): Promise<GameEntity[]>{
-        return await this.gameService.index(id);
-    }
-
-    /* GET ALL MY GAMES [ ALL ]*/
-    @Get('')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Get all my games  [ONLY DEV]' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Developer)
-    async getMyGames(@GetUser() user: UserEntity): Promise<GameEntity[]>{
-        return await this.gameService.index(user);
-    }
-
-     /* DELETE A GAME [ONLY DEV]*/
-    @Delete(':id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Delete a game [ONLY DEV] ' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Developer, Roles.Admin)
-    async delete(
-        @Param('id') id: number,
-        @GetUser() user: UserEntity): Promise<GameEntity[]>{
-        return await this.gameService.delete(user, id);
-    }
-
-    
-     /* CHECK GAME PROPERTY */
-    @Get('my/:id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Check Game property [ONLY DEV]' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Developer)
-    async check(
-        @Param('id') id: number,
-        @GetUser() user: UserEntity): Promise<GameEntity>{
-        return await this.gameService.check(id, user);
-    }
-
-    /* CHANGE STATUS [ONLY ADMIN] */
-    @Put('status/:id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Change Status [ONLY ADMIN]' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Admin)
-    async changeStatus(
-        @Param('id') id: number,
-        @GetUser() user: UserEntity): Promise<GameEntity>{
-        return await this.gameService.changeSatus(user, id);
-    }
+  /* STORE  [ ONLY DEV ]*/
+  @Post('')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ summary: 'Create a game [ONLY DEV]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'image', maxCount: 1 },
+        { name: 'icon', maxCount: 1 },
+        { name: 'screenshots', maxCount: 5 },
+      ],
+      ValidatedFileConfig,
+    ),
+  )
+  @UploadFileNestjs('image', 'icon', 'screenshots')
+  async store(
+    @Request() req: any,
+    @Body() CreateGameDto: CreateGameDto,
+    @GetUser() user: UserEntity,
+  ): Promise<any> {
+    return await this.gameService.store(CreateGameDto, user, req);
+  }
 
 
-    /* UPDATE GAME  [ ONLY DEV ]*/
-    @Patch(':id')
-    @ApiBasicAuth('XYZ')
-    @ApiBearerAuth()
-    @ApiOperation({ summary: 'Update a game [ONLY DEV]' })
-    @UseGuards(RolesGuard)
-    @UseGuards(JwtAuthGuard)
-    @hasRoles(Roles.Developer)
-    @UseInterceptors(FileFieldsInterceptor([
-            { name: 'image', maxCount: 1, },
-            { name: 'icon', maxCount: 1, },
-            { name: 'screenshots', maxCount: 5 }
-        ],
-         ValidatedFileConfig
-    ))
-    @UploadFileNestjs('image')
-    async update(
-        @Param('id') id: number,
-        @Body() UpdateGameDto: UpdateGameDto,
-        @GetUser() user: UserEntity
-    ): Promise<any>{
-        return await this.gameService.update(UpdateGameDto, user, id);
-    }
-    
+  @Post('paginate')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all my games paginate  [ONLY DEV]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer)
+  async getMyGamesPaginate(
+    @GetUser() user: UserEntity,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 9,
+    @Body() filtros: any,
+  ): Promise<Pagination<GameEntity>> {
+    console.log(filtros);
+    limit = limit > 100 ? 100 : limit;
+    return await this.gameService.indexPaginate(user, page, limit, filtros);
+  }
+
+  /* GET [ ALL ]*/
+  @Get(':id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get a game [ALL]' })
+  async get(@Param('id') id: number): Promise<GameEntity> {
+    return await this.gameService.get(id);
+  }
+
+  /* GET GAME USERS [ ALL ]*/
+  @Get('user/:id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all games from user [ALL]' })
+  async getGamesFromDev(@Param('id') id: UserEntity): Promise<GameEntity[]> {
+    return await this.gameService.index(id);
+  }
+
+  /* GET ALL MY GAMES [ ALL ]*/
+  @Get('')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all my games  [ONLY DEV]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer)
+  async getMyGames(@GetUser() user: UserEntity): Promise<GameEntity[]> {
+    return await this.gameService.index(user);
+  }
+
+  /* DELETE A GAME [ONLY DEV]*/
+  @Delete(':id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a game [ONLY DEV] ' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer, Roles.Admin)
+  async delete(
+    @Param('id') id: number,
+    @GetUser() user: UserEntity,
+  ): Promise<GameEntity[]> {
+    return await this.gameService.delete(user, id);
+  }
+
+  /* CHECK GAME PROPERTY */
+  @Get('my/:id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Check Game property [ONLY DEV]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer)
+  async check(
+    @Param('id') id: number,
+    @GetUser() user: UserEntity,
+  ): Promise<GameEntity> {
+    return await this.gameService.check(id, user);
+  }
+
+  /* CHANGE STATUS [ONLY ADMIN] */
+  @Put('status/:id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Change Status [ONLY ADMIN]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Admin)
+  async changeStatus(
+    @Param('id') id: number,
+    @GetUser() user: UserEntity,
+  ): Promise<GameEntity> {
+    return await this.gameService.changeSatus(user, id);
+  }
+
+  /* UPDATE GAME  [ ONLY DEV ]*/
+  @Patch(':id')
+  @ApiBasicAuth('XYZ')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update a game [ONLY DEV]' })
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @hasRoles(Roles.Developer)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'image', maxCount: 1 },
+        { name: 'icon', maxCount: 1 },
+        { name: 'screenshots', maxCount: 5 },
+      ],
+      ValidatedFileConfig,
+    ),
+  )
+  @UploadFileNestjs('image')
+  async update(
+    @Param('id') id: number,
+    @Body() UpdateGameDto: UpdateGameDto,
+    @GetUser() user: UserEntity,
+  ): Promise<any> {
+    return await this.gameService.update(UpdateGameDto, user, id);
+  }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, forwardRef, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { UploadFileDto } from 'src/file/dto/uploadFile.dto';
 import { FileService } from 'src/file/file.service';
@@ -7,118 +13,140 @@ import { UserEntity } from './../user/entities/user.entity';
 import { CreateGameDto } from './dto/createGame.dto';
 import { UpdateGameDto } from './dto/updateGame.dto';
 import { GameEntity } from './entities/game.entity';
-
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class GameService {
-    constructor(
-        @Inject('MICRO-DEV') private microDev: ClientProxy,
-        private readonly categoryService: CategoryService,
-        @Inject(forwardRef(() => FileService)) private readonly fileService: FileService,
+  constructor(
+    @Inject('MICRO-DEV') private microDev: ClientProxy,
+    private readonly categoryService: CategoryService,
+    @Inject(forwardRef(() => FileService))
+    private readonly fileService: FileService,
+  ) {}
 
-    ) { }
-    
-    /* CREATE GAME */
-    async store(
-        createGameDto: CreateGameDto,
-        user: UserEntity,
-        req: any,
-    ): Promise<any> {
-        if (req.fileValidationError) {
-            throw new BadRequestException('El formato del archivo no es aceptable')
-        }
-        const { category_id } = createGameDto;
-        await this.categoryService.get(category_id)
+  /* CREATE GAME */
+  async store(
+    createGameDto: CreateGameDto,
+    user: UserEntity,
+    req: any,
+  ): Promise<any> {
+    if (req.fileValidationError) {
+      throw new BadRequestException('El formato del archivo no es aceptable');
+    }
+    const { category_id } = createGameDto;
+    await this.categoryService.get(category_id);
 
-        return this.microDev.send(
-            { cmd: 'game_store' },
-            { createGameDto, user });
-    }
+    return this.microDev.send({ cmd: 'game_store' }, { createGameDto, user });
+  }
 
-    /* GET  GAME */
-    async get(id: number): Promise<GameEntity> {
-        const game = await this.microDev.send({ cmd: 'game_get' }, { id }).toPromise();
-        if (!game) {
-            throw new NotFoundException(`Game with ${id} not found`)
-        }
-        return await this.gameCategory(game);
-    }
+  async indexPaginate(
+    user: UserEntity,
+    page,
+    limit,
+    filtros,
+  ): Promise<Pagination<GameEntity>> {
+    return  await this.microDev
+      .send({ cmd: 'game_user_paginate' }, { user, page, limit, filtros })
+      .toPromise();
+  }
 
-    /* GET ALL GAMES FROM USER */
-    async index(user: UserEntity): Promise<GameEntity[]> {
-        const games: GameEntity[] = await this.microDev.send({ cmd: 'game_user' }, { user }).toPromise();
-        return await this.gameCategory(games);
+  /* GET  GAME */
+  async get(id: number): Promise<GameEntity> {
+    const game = await this.microDev
+      .send({ cmd: 'game_get' }, { id })
+      .toPromise();
+    if (!game) {
+      throw new NotFoundException(`Game with ${id} not found`);
     }
+    return await this.gameCategory(game);
+  }
 
-    /* UPDATE A GAME */
-    async update(
-        updateGameDto: UpdateGameDto,
-        user: UserEntity,
-        id: number): Promise<any> {
-        const game = await this.check(id, user);
-        return await this.microDev.send({ cmd: 'game_update' }, { updateGameDto, user, game }).toPromise()
-    }
+  /* GET ALL GAMES FROM USER */
+  async index(user: UserEntity): Promise<GameEntity[]> {
+    const games: GameEntity[] = await this.microDev
+      .send({ cmd: 'game_user' }, { user })
+      .toPromise();
+    return await this.gameCategory(games);
+  }
 
-    /* DELETE A GAME */
-    async delete(
-        user: UserEntity,
-        id: number): Promise<any> {
-        const game = await this.check(id, user);
-        return await this.microDev.send({ cmd: 'game_delete' }, { user, game }).toPromise();
-    }
-    
-    /* CHANGE STATUS */
-    async changeSatus(
-        user: UserEntity,
-        id: number): Promise<any> {
-        const game = await this.get(id);
-        return await this.microDev.send({ cmd: 'game_change_status' }, { user, game }).toPromise();
-    }
+  /* UPDATE A GAME */
+  async update(
+    updateGameDto: UpdateGameDto,
+    user: UserEntity,
+    id: number,
+  ): Promise<any> {
+    const game = await this.check(id, user);
+    return await this.microDev
+      .send({ cmd: 'game_update' }, { updateGameDto, user, game })
+      .toPromise();
+  }
 
-    /* CHECK PROPERTY A GAME */
-    async check(
-        id: number,
-        user: UserEntity,
-    ): Promise<GameEntity> {
-        const game = await this.microDev.send(
-            { cmd: 'game_check_property' },
-            { user, id }).toPromise();
-        if (!game) {
-            throw new NotFoundException(`This game is not your property`);
-        }
-        return game;
-    }
+  /* DELETE A GAME */
+  async delete(user: UserEntity, id: number): Promise<any> {
+    const game = await this.check(id, user);
+    return await this.microDev
+      .send({ cmd: 'game_delete' }, { user, game })
+      .toPromise();
+  }
 
-    /* GET ALL GAMES */
-    async all() {
-        return this.microDev.send({ cmd: 'game_all' }, {});
-    }
+  /* CHANGE STATUS */
+  async changeSatus(user: UserEntity, id: number): Promise<any> {
+    const game = await this.get(id);
+    return await this.microDev
+      .send({ cmd: 'game_change_status' }, { user, game })
+      .toPromise();
+  }
 
-    /* ADD IMAGE FOR GAME */
-    async addImage(uploadFileDto: UploadFileDto, user: UserEntity, image?: any, icon?: any, screenshots?: any[]): Promise<any>{
-        const { entity_id } = uploadFileDto;
-        const game = await this.check(entity_id, user);
-        const response =  await this.microDev.send(
-            { cmd: 'game_add_image' },
-            { uploadFileDto, game, user, image, icon, screenshots }
-        ).toPromise();
-        await this.fileService.removeFiles(response[0].image_before);
-        await this.fileService.removeFiles(response[1].screenshots_before);
-        return response;
+  /* CHECK PROPERTY A GAME */
+  async check(id: number, user: UserEntity): Promise<GameEntity> {
+    const game = await this.microDev
+      .send({ cmd: 'game_check_property' }, { user, id })
+      .toPromise();
+    if (!game) {
+      throw new NotFoundException(`This game is not your property`);
     }
+    return game;
+  }
 
-    /* GET CAME WITH CATEGORY DATA */
-    async gameCategory(games: any): Promise<any> {
-        const games_array: GameEntity[] = [];
-        for (let game of games) {
-            const category_id = game.category_id;
-            const category = await this.categoryService.get(category_id);
-            game.category_id = category;
-            games_array.push(game);
-        }
-        return games_array
+  /* GET ALL GAMES */
+  async all() {
+    return this.microDev.send({ cmd: 'game_all' }, {});
+  }
+
+  /* ADD IMAGE FOR GAME */
+  async addImage(
+    uploadFileDto: UploadFileDto,
+    user: UserEntity,
+    image?: any,
+    icon?: any,
+    screenshots?: any[],
+  ): Promise<any> {
+    const { entity_id } = uploadFileDto;
+    const game = await this.check(entity_id, user);
+    const response = await this.microDev
+      .send(
+        { cmd: 'game_add_image' },
+        { uploadFileDto, game, user, image, icon, screenshots },
+      )
+      .toPromise();
+    await this.fileService.removeFiles(response[0].image_before);
+    await this.fileService.removeFiles(response[1].screenshots_before);
+    return response;
+  }
+
+  /* GET CAME WITH CATEGORY DATA */
+  async gameCategory(games: any): Promise<any> {
+    const games_array: GameEntity[] = [];
+    for (let game of games) {
+      const category_id = game.category_id;
+      const category = await this.categoryService.get(category_id);
+      game.category_id = category;
+      games_array.push(game);
     }
-    
+    return games_array;
+  }
 }
-
-
