1e6bbc7594d4dbe1f43edccc65934c14e1ba2c9e
Ruben Castañeda <ruben@pixeling.co>
2021-09-16 19:05:18 -0500

    FEAT(Events)PaginateAndFilters

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -6,85 +6,95 @@ import { UserEntity } from 'src/user/entities/user.entity';
 import { CreateEventDto } from './dto/createEvent.dto';
 import { UpdateEventDto } from './dto/updateEvent.dto';
 import { EventEntity } from './entities/event.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class EventService {
-    constructor(
-        @Inject('MICRO-DEV') private microDev: ClientProxy,
-        private readonly gameService: GameService,
-        private readonly gameUserService: GameUserService,
-    ) { }
-    
-    /* STORE */
-    async store(
-        createEventDto: CreateEventDto,
-        user: UserEntity,
-    ): Promise<any>{
-        const { game_id } = createEventDto;
-        const game = await this.gameService.check(game_id, user);
-        return await this.microDev.send(
-            { cmd: 'event_store' },
-            { createEventDto, game, user }
-        ).toPromise();
-    }
+  constructor(
+    @Inject('MICRO-DEV') private microDev: ClientProxy,
+    private readonly gameService: GameService,
+    private readonly gameUserService: GameUserService,
+  ) {}
 
-    /* GET ALL EVENTS CREATED */
-    async index(user: UserEntity): Promise<EventEntity[]>{
-        return await this.microDev.send({ cmd: 'event_index' }, { user }).toPromise();
-    }
+  /* STORE */
+  async store(createEventDto: CreateEventDto, user: UserEntity): Promise<any> {
+    const { game_id } = createEventDto;
+    const game = await this.gameService.check(game_id, user);
+    return await this.microDev
+      .send({ cmd: 'event_store' }, { createEventDto, game, user })
+      .toPromise();
+  }
 
-    /* GET EVENT WITH ID*/
-    async get(id: number): Promise<EventEntity>{
-        const event = await this.microDev.send({ cmd: 'event_get' }, { id }).toPromise();
-        if(!event){
-            throw new NotFoundException(`The event with id ${id} not found`);
-        }
-        return event;
-    }
+  async indexPaginate(
+    user: UserEntity,
+    page,
+    limit,
+    filtros,
+  ): Promise<Pagination<EventEntity[]>> {
+    return await this.microDev
+      .send({ cmd: 'event_index_paginate' }, { user, page, limit, filtros })
+      .toPromise();
+  }
 
-    /* GET FROM GAME */
-    async getFromGames(game_id: number): Promise<EventEntity[]>{
-        const game = await this.gameService.get(game_id);
-        return await this.microDev.send({ cmd: 'event_get_from_game' }, { game }).toPromise();
-    }
+  /* GET ALL EVENTS CREATED */
+  async index(user: UserEntity): Promise<EventEntity[]> {
+    return await this.microDev
+      .send({ cmd: 'event_index' }, { user })
+      .toPromise();
+  }
 
-    /* CHECK */
-    async check(id: number, user: UserEntity): Promise<EventEntity>{
-        const event = await this.microDev.send({ cmd: 'event_check' }, { id, user }).toPromise();
-        if (!event) {
-            throw new NotFoundException(`This event is not your property`);
-        }
-        return event;
+  /* GET EVENT WITH ID*/
+  async get(id: number): Promise<EventEntity> {
+    const event = await this.microDev
+      .send({ cmd: 'event_get' }, { id })
+      .toPromise();
+    if (!event) {
+      throw new NotFoundException(`The event with id ${id} not found`);
     }
+    return event;
+  }
 
-    /* UPDATE */
-    async update(
-        updateEventDto: UpdateEventDto,
-        user: UserEntity,
-        id: number
-    ): Promise<any> {
-        const event = await this.check(id, user);
-        const { game_id } = updateEventDto;
-        let game = null;
-        if (game_id) {
-            game = await this.gameService.check(game_id, user);
-        }
-        return await this.microDev.send(
-            { cmd: 'event_update' },
-            { updateEventDto, user, game, event }
-        ).toPromise();
-    }
+  /* GET FROM GAME */
+  async getFromGames(game_id: number): Promise<EventEntity[]> {
+    const game = await this.gameService.get(game_id);
+    return await this.microDev
+      .send({ cmd: 'event_get_from_game' }, { game })
+      .toPromise();
+  }
 
-    /* DELETE */
-    async delete(
-        user: UserEntity,
-        id: number,
-    ): Promise<any>{
-        const event = await this.check(id, user);
-        return await this.microDev.send(
-            { cmd: 'event_delete'},
-            { event, user }
-        ).toPromise();
+  /* CHECK */
+  async check(id: number, user: UserEntity): Promise<EventEntity> {
+    const event = await this.microDev
+      .send({ cmd: 'event_check' }, { id, user })
+      .toPromise();
+    if (!event) {
+      throw new NotFoundException(`This event is not your property`);
     }
+    return event;
+  }
+
+  /* UPDATE */
+  async update(
+    updateEventDto: UpdateEventDto,
+    user: UserEntity,
+    id: number,
+  ): Promise<any> {
+    const event = await this.check(id, user);
+    const { game_id } = updateEventDto;
+    let game = null;
+    if (game_id) {
+      game = await this.gameService.check(game_id, user);
+    }
+    return await this.microDev
+      .send({ cmd: 'event_update' }, { updateEventDto, user, game, event })
+      .toPromise();
+  }
+
+  /* DELETE */
+  async delete(user: UserEntity, id: number): Promise<any> {
+    const event = await this.check(id, user);
+    return await this.microDev
+      .send({ cmd: 'event_delete' }, { event, user })
+      .toPromise();
+  }
 }
-
