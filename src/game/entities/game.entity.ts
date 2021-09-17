04b2b57186d5a0dcf34f030694431c3e036562b2
jorbinogales <jorbinogales@gmail.com>
2021-09-17 12:37:25 -0400

    Fix: EventsService
    Se corrigio el error del evento

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,5 +1,8 @@
 import { AchievementEntity } from "src/achievement/entities/achievement.entity";
 import { CategoryEntity } from "src/category/entities/category.entity";
+import { EventEntity } from "src/event/entities/event.entity";
+import { GameUserEntity } from "src/game-user/entities/gameUser.entity";
+import { LeaderBoardEntity } from "src/leaderboard/entities/leaderboard.entity";
 import { UserEntity } from "src/user/entities/user.entity";
 import {
     Entity
@@ -12,6 +15,9 @@ export class GameEntity {
     category_id: number;
     achievements_value: AchievementEntity[];
     screenshots_value: [];
+    events_value: EventEntity[];
+    leaderboards_value: LeaderBoardEntity[];
+    users_value: GameUserEntity[];
     title: string;
     description: string;
     icon: string;
